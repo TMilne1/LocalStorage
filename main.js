7d8000ab8f0a55fb2f696daa53085711fd734396
tmilne1 <tmilne1@gmail.com>
2019-12-23 21:35:11 -0500

    ability to add to list and check off items

--- main.js
+++ main.js
@@ -3,15 +3,28 @@ const itemsList = document.querySelector('.plates');
 const items = [];
 
 function addItem(event){
-    event.preventDefault();
-    const text = (this.querySelector('[name =item')).value;
+    event.preventDefault(); // prevent page from refreshing
+    const text = (this.querySelector('[name =item]')).value;
     const item  = {
-        text,
+        text ,
         done:false
     }
     items.push(item);
+    populateList(items, itemsList)
     this.reset();
+}
 
+
+function populateList(plates = [], platesList) {
+    
+    platesList.innerHTML = plates.map((plate, index) => {
+        return `
+    <li>
+        <input type ='checkbox' data-index = "${index}"  id = "item${index}"  "${plate.done ? 'checked' : ''}/ >
+        <label for="item${index}">${plate.text}</label>
+     </li>
+    `;
+    }).join("");
 }
 
 
