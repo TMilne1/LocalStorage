a67d26b210a01dc6887c0b6e9737deab6cd3b9d7
tmilne1 <tmilne1@gmail.com>
2019-12-23 21:10:15 -0500

    added ability to populate items array via input field

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(event){
+    event.preventDefault();
+    const text = (this.querySelector('[name =item')).value;
+    const item  = {
+        text,
+        done:false
+    }
+    items.push(item);
+    this.reset();
+
+}
+
+
+addItems.addEventListener('submit', addItem);
